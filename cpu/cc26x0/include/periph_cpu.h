3f29876a4eb8b27dc0d6d439d900ed8e30a5bdb5
Leon M. George <leon@georgemail.eu>
2016-01-23 14:38:51 +0100

    cpu: add CC26x0-family

---
+++ cpu/cc26x0/include/periph_cpu.h
@@ -0,0 +1,27 @@
+/**
+ * @ingroup         cpu_cc26x0
+ * @{
+ *
+ * @file
+ * @brief           CPU specific definitions for internal peripheral handling
+ *
+ * @author          Hauke Petersen <hauke.peterse@fu-berlin.de>
+ */
+
+#ifndef PERIPH_CPU_H_
+#define PERIPH_CPU_H_
+
+#include "periph/dev_enums.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* nothing to do here, yet */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PERIPH_CPU_H_ */
+/** @} */
